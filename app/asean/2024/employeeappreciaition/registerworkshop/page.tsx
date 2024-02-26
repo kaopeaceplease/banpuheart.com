d122a717a28f7b7f7c8b06db36c0a5b3bc2afae8
Sarunyhot Suvannachoti <kao@peacepleasestudio.com>
2024-02-26 16:49:32 +0700

    Embeded Typeform

--- app/asean/2024/employeeappreciaition/registerworkshop/page.tsx
+++ app/asean/2024/employeeappreciaition/registerworkshop/page.tsx
@@ -1,12 +1,6 @@
+"use client";
+import { Widget } from "@typeform/embed-react";
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">Life at Nissan</h1>
-        <p className="text-lg text-gray-700">
-          This is page for Register Virtual Workshop (Typeform Embed Fullscreen)
-        </p>
-      </div>
-    </div>
-  );
+  return <Widget id="FbBUET2x" fullScreen />;
 }
