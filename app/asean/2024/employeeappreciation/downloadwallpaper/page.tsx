6a3a49528a49b0a9f97c6b88eb1273863032720b
Sarunyhot Suvannachoti <kao@peacepleasestudio.com>
2024-03-01 12:55:52 +0700

    Added wallpapers to download

--- app/asean/2024/employeeappreciation/downloadwallpaper/page.tsx
+++ app/asean/2024/employeeappreciation/downloadwallpaper/page.tsx
@@ -1,11 +1,45 @@
-export default function Home() {
+import Image from "next/image";
+
+export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="mb-4 text-4xl font-bold">Life at Nissan</h1>
-        <p className="text-lg text-gray-700">
-          This is page for Digital Affirmation Card
-        </p>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <div className="mb-8">
+          <Image
+            src="/images/wallpaper/nissan-wallpaper-01.png"
+            alt="nissan wallpaper"
+            width={500}
+            height={900}
+            className="rounded-lg shadow-md"
+          />
+        </div>
+        <div className="mb-8">
+          <Image
+            src="/images/wallpaper/nissan-wallpaper-02.png"
+            alt="nissan wallpaper"
+            width={500}
+            height={900}
+            className="rounded-lg shadow-md"
+          />
+        </div>
+        <div className="mb-8">
+          <Image
+            src="/images/wallpaper/nissan-wallpaper-03.png"
+            alt="nissan wallpaper"
+            width={500}
+            height={900}
+            className="rounded-lg shadow-md"
+          />
+        </div>
+        <div className="mb-8">
+          <Image
+            src="/images/wallpaper/nissan-wallpaper-04.png"
+            alt="nissan wallpaper"
+            width={500}
+            height={900}
+            className="rounded-lg shadow-md"
+          />
+        </div>
       </div>
     </div>
   );
