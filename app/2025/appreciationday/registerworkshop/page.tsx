60616c5df9a76f0daef400d9efe617893dbfb659
Sarunyhot Suvannachoti <kao@peacepleasestudio.com>
2025-02-18 01:09:14 +0700

    chore: remove unused wallpaper and workshop registration pages; update homepage redirect

--- app/2025/appreciationday/registerworkshop/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { Widget } from "@typeform/embed-react";
-
-export default function Home() {
-  return <Widget id="JQgBUZY8" fullScreen />;
-}
--- app/2025/appreciationday/specialgift/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { Widget } from "@typeform/embed-react";
-
-export default function Home() {
-  return <Widget id="R4nmgy3V" fullScreen />;
-}
